36874007f0f305b897e9384a65da1fdc1e1f5c22
Jim Cramer <remarcmij@gmail.com>
2021-01-18 10:07:38 +0100

    Improves exercise and unit test

--- 1-Javascript/Week3/homework/ex2-dogYears.js
+++ 1-Javascript/Week3/homework/ex2-dogYears.js
@@ -1,26 +1,26 @@
 "use strict";
-/*
- * You know how old your dog is in human years, but what about dog years? Let's calculate it!
- *
- * Write a function named calculateDogAge.
- *   It takes 1 argument: your (fictional) puppy's age (number).
- *   Calculate your dog's age based on the conversion rate of 1 human year to 7 dog years.
- *   Return a string: "Your doggie is [CALCULATED_VALUE] years old in dog years!"
- *   Call the function three times with different sets of values.
- *
- * Expected output:
- *   calculateDogAge(1); // Returns "Your doggie is 7 years old in dog years!"
- *   calculateDogAge(2); // Returns "Your doggie is 14 years old in dog years!"
- *   calculateDogAge(3); // Returns "Your doggie is 21 years old in dog years!"
- */
+/*------------------------------------------------------------------------------
+You know how old your dog is in human years, but what about dog years? Let's 
+calculate it!
+
+1. Write a function named `calculateDogAge`.
+
+  - It takes one argument: your (fictional) puppy's age (number).
+  - Calculate your dog's age based on the conversion rate of 1 human year to 
+    7 dog years.
+  - Return a string: "Your doggie is `age` years old in dog years!"
+
+2. Use `console.log` to display the result of the function for three different 
+   ages.
+-----------------------------------------------------------------------------*/
 
 function calculateDogAge(/* parameters go here */) {
-  // replace this comment with your code
+  // TODO: replace this comment with your code
 }
 
 console.log(calculateDogAge(1));
 console.log(calculateDogAge(2));
 console.log(calculateDogAge(3));
 
-// Do not change or remove the code below
+// ! Do not change or remove the code below
 module.exports = calculateDogAge;
--- 1-Javascript/Week3/unit-tests/ex2-dogYears.test.js
+++ 1-Javascript/Week3/unit-tests/ex2-dogYears.test.js
@@ -11,17 +11,21 @@ describe("calculateDogAge", () => {
     calculateDogAge = exports;
   });
 
-  it("1 human year --> 7 dog years", () => {
+  it("should take a single parameter", () => {
+    expect(calculateDogAge).toHaveLength(1);
+  });
+
+  it("should give 7 dog years for 1 human year", () => {
     expect(calculateDogAge(1)).toBe("Your doggie is 7 years old in dog years!");
   });
 
-  it("2 human years --> 14 dog years", () => {
+  it("should give 14 dog years for 2 human years", () => {
     expect(calculateDogAge(2)).toBe(
       "Your doggie is 14 years old in dog years!"
     );
   });
 
-  it("3 human years --> 21 dog years", () => {
+  it("give 21 dog years for 3 human years", () => {
     expect(calculateDogAge(3)).toBe(
       "Your doggie is 21 years old in dog years!"
     );
