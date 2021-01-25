716ab8ae2d1c178a9a58e656a04f913caffb1bc5
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2021-01-25 21:54:03 +0100

    more use cases

--- 1-JavaScript/Week3/homework/ex1-giveCompliment.js
+++ 1-JavaScript/Week3/homework/ex1-giveCompliment.js
@@ -4,17 +4,17 @@
 1. Complete the function named `giveCompliment`as follows:
 
    - It should take a single argument: `name`.
-   - Its function body should include a variable that holds an array, 
-     `compliments`, initialized with 10 strings. Each string should be a 
+   - Its function body should include a variable that holds an array,
+     `compliments`, initialized with 10 strings. Each string should be a
      compliment, like `"great"`, `"awesome"` and so on.
    - It should randomly select a compliment from the array.
-   - It should return the string "You are `compliment`, `name`!", where 
-     `compliment` is a randomly selected compliment and `name` is the name that 
+   - It should return the string "You are `compliment`, `name`!", where
+     `compliment` is a randomly selected compliment and `name` is the name that
      was passed as the function's argument.
 
-2. Call the function three times, giving each function call the same argument: 
-   your name. 
-   Use `console.log` each time to display the return value of the 
+2. Call the function three times, giving each function call the same argument:
+   your name.
+   Use `console.log` each time to display the return value of the
    `giveCompliment` function to the console.
 -----------------------------------------------------------------------------*/
 function giveCompliment(/* TODO: parameter(s) go here */) {
@@ -27,6 +27,12 @@ const myName = "HackYourFuture";
 console.log(giveCompliment(myName));
 console.log(giveCompliment(myName));
 console.log(giveCompliment(myName));
+
+const yourName = "Amsterdam";
+
+console.log(giveCompliment(yourName));
+console.log(giveCompliment(yourName));
+console.log(giveCompliment(yourName));
 
 // ! Do not change or remove the code below
 module.exports = giveCompliment;
--- 1-JavaScript/Week4/homework/ex4-publishSubscribe.js
+++ 1-JavaScript/Week4/homework/ex4-publishSubscribe.js
@@ -1,37 +1,37 @@
 "use strict";
 /*
-A software pattern that you may encounter in the future is a construct called 
-the Observer Pattern. It enables "listeners" (which are usually functions) to 
-"subscribe" to "notifications" from a "publisher". Any number of listeners can 
+A software pattern that you may encounter in the future is a construct called
+the Observer Pattern. It enables "listeners" (which are usually functions) to
+"subscribe" to "notifications" from a "publisher". Any number of listeners can
 subscribe.
 
 Look at the code below:
 
-- The call to the `createPublisher` function returns an object. For ease of 
+- The call to the `createPublisher` function returns an object. For ease of
   reference, let's call it a "Publisher" object here. The Publisher object has
-  two properties, `subscribe` and `notify`, which are both functions. In this 
+  two properties, `subscribe` and `notify`, which are both functions. In this
   exercise you are required to complete them. (But continue reading first.)
 
-- As you can see below, the `createPublisher` function is called and the 
+- As you can see below, the `createPublisher` function is called and the
   resulting Publisher object is assigned to the `myPublisher` variable.
 
-- Next, two "listener" functions are defined, notably `consoleUpperCase` and 
-  `consoleLowerCase`. A listener function is defined here as a function that 
-  takes a single parameter, `message`. It is up to the listener what to do 
+- Next, two "listener" functions are defined, notably `consoleUpperCase` and
+  `consoleLowerCase`. A listener function is defined here as a function that
+  takes a single parameter, `message`. It is up to the listener what to do
   with `message`. (The Publisher has no say in this!).
 
-- The "listener" functions are added as "subscribers" to `myPublisher` by 
-  calling its `subscribe` function. The `subscribe` function should take the 
-  function passed to it as an argument and push it onto the `listeners` array. 
+- The "listener" functions are added as "subscribers" to `myPublisher` by
+  calling its `subscribe` function. The `subscribe` function should take the
+  function passed to it as an argument and push it onto the `listeners` array.
   (Yes, you can store functions in an array. Functions are treated in JavaScript
   like any other value.
 
-- The standard `console.log` function, which also conforms to the minimum 
-  requirement for a "listener" (although it can take more than one argument) 
+- The standard `console.log` function, which also conforms to the minimum
+  requirement for a "listener" (although it can take more than one argument)
   is also added as a subscriber.
 
-- Finally, a call to the Publisher's `notify` function is expected to iterate 
-  through, and call, all subscribers from the `listeners` array, passing on the 
+- Finally, a call to the Publisher's `notify` function is expected to iterate
+  through, and call, all subscribers from the `listeners` array, passing on the
   notification message to each listener.
 
 Good luck with completing `createPublisher`!
@@ -68,6 +68,17 @@ myPublisher.notify("Let's see what happens here!");
 // "Let's see what happens here!"
 // "LET'S SEE WHAT HAPPENS HERE!"
 // "let's see what happens here!"
+
+myPublisher.subscribe(function consoleReverse(message) {
+  console.log(message.split("").reverse().join(""));
+});
+
+myPublisher.notify("Beware the closed Observer 0_o");
+// Prints the following to the console
+// "Beware the closed Observer 0_o!"
+// "BEWARE THE CLOSED OBSERVER 0_O!"
+// "beware the closed observer 0_o!"
+// "!o_0 revresbO desolc eht eraweB"
 
 // ! Do not change or remove the code below
 module.exports = createPublisher;
