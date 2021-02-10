b0cf60dbb1f874a1862f4ee5c21f793f78eb1f1f
Burak <isikburak142@gmail.com>
2021-02-10 13:20:39 +0100

    The problem about the test is solved thanks to Jim help.

--- 1-JavaScript/Week3/homework/ex1-giveCompliment.js
+++ 1-JavaScript/Week3/homework/ex1-giveCompliment.js
@@ -17,12 +17,26 @@
    Use `console.log` each time to display the return value of the
    `giveCompliment` function to the console.
 -----------------------------------------------------------------------------*/
-function giveCompliment(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function giveCompliment(name) {
+  const compliments = [
+    'great',
+    'awesome',
+    'wonderful',
+    'small',
+    'odd',
+    'strange',
+    'normal',
+    'clean',
+    'dirty',
+    'word',
+  ];
+  const compliment = Math.floor(Math.random() * compliments.length);
+  const result = compliments[compliment];
+  return `You are ${result}, ${name}!`;
 }
 
 // TODO substitute your own name for "HackYourFuture"
-const myName = 'HackYourFuture';
+const myName = 'Burak';
 
 console.log(giveCompliment(myName));
 console.log(giveCompliment(myName));
