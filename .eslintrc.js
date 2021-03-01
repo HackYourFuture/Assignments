bc22cf84706f65de49dcafe697eefc8e509a639b
Jim Cramer <remarcmij@gmail.com>
2021-03-01 17:07:30 +0100

    Removes literal index rule; adds innerText rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,162 @@ module.exports = {
         message: 'Results from `map` are unused. Replace `map` with `forEach`.',
       },
       {
+        selector: "MemberExpression[property.name='innerText']",
+        message:
+          'The homework tests do not support `innerText`. Please replace with `textContent`.',
+      },
+      module.exports = {
+        env: {
+          browser: true,
+          commonjs: true,
+          es6: true,
+          node: true,
+          jest: true,
+        },
+        plugins: ['hyf', 'no-autofix'],
+        extends: ['eslint:recommended'],
+        parserOptions: {
+          ecmaVersion: 2020,
+        },
+        globals: {
+          page: 'readonly',
+          browser: 'readonly',
+          context: 'readonly',
+          jestPuppeteer: 'readonly',
+          axios: 'readonly',
+        },
+        rules: {
+          'no-console': 'off',
+          'no-var': 'error',
+          'prefer-const': 'off',
+          'no-autofix/prefer-const': 'warn',
+          'new-cap': 'error',
+          'no-useless-computed-key': 'error',
+          eqeqeq: 'error',
+          'no-restricted-syntax': [
+            'warn',
+            {
+              selector:
+                "ExpressionStatement > CallExpression > MemberExpression > Identifier[name='map']",
+              message: 'Results from `map` are unused. Replace `map` with `forEach`.',
+            },
+            {
+              selector: "MemberExpression[property.name='innerText']",
+              message:
+                'The homework tests do not support `innerText`. Please replace with `textContent`.',
+            },
+            {
+              // disallow numeric literals as array indices (zero is allowed)
+              selector: 'MemberExpression[computed] > Literal[value>0]',
+              message: 'No literal',
+            },
+            { selector: 'ForInStatement', message: 'Avoid `for in` loops' },
+          ],
+          'hyf/use-map-result': 'error',
+          'hyf/camelcase': 'warn',
+          'hyf/no-commented-out-code': 'warn',
+        },
+      };
+      module.exports = {
+        env: {
+          browser: true,
+          commonjs: true,
+          es6: true,
+          node: true,
+          jest: true,
+        },
+        plugins: ['hyf', 'no-autofix'],
+        extends: ['eslint:recommended'],
+        parserOptions: {
+          ecmaVersion: 2020,
+        },
+        globals: {
+          page: 'readonly',
+          browser: 'readonly',
+          context: 'readonly',
+          jestPuppeteer: 'readonly',
+          axios: 'readonly',
+        },
+        rules: {
+          'no-console': 'off',
+          'no-var': 'error',
+          'prefer-const': 'off',
+          'no-autofix/prefer-const': 'warn',
+          'new-cap': 'error',
+          'no-useless-computed-key': 'error',
+          eqeqeq: 'error',
+          'no-restricted-syntax': [
+            'warn',
+            {
+              selector:
+                "ExpressionStatement > CallExpression > MemberExpression > Identifier[name='map']",
+              message: 'Results from `map` are unused. Replace `map` with `forEach`.',
+            },
+            {
+              selector: "MemberExpression[property.name='innerText']",
+              message:
+                'The homework tests do not support `innerText`. Please replace with `textContent`.',
+            },
+            {
+              // disallow numeric literals as array indices (zero is allowed)
+              selector: 'MemberExpression[computed] > Literal[value>0]',
+              message: 'No literal',
+            },
+            { selector: 'ForInStatement', message: 'Avoid `for in` loops' },
+          ],
+          'hyf/use-map-result': 'error',
+          'hyf/camelcase': 'warn',
+          'hyf/no-commented-out-code': 'warn',
+        },
+      };
+      module.exports = {
+        env: {
+          browser: true,
+          commonjs: true,
+          es6: true,
+          node: true,
+          jest: true,
+        },
+        plugins: ['hyf', 'no-autofix'],
+        extends: ['eslint:recommended'],
+        parserOptions: {
+          ecmaVersion: 2020,
+        },
+        globals: {
+          page: 'readonly',
+          browser: 'readonly',
+          context: 'readonly',
+          jestPuppeteer: 'readonly',
+          axios: 'readonly',
+        },
+        rules: {
+          'no-console': 'off',
+          'no-var': 'error',
+          'prefer-const': 'off',
+          'no-autofix/prefer-const': 'warn',
+          'new-cap': 'error',
+          'no-useless-computed-key': 'error',
+          eqeqeq: 'error',
+          'no-restricted-syntax': [
+            'warn',
+            {
+              selector:
+                "ExpressionStatement > CallExpression > MemberExpression > Identifier[name='map']",
+              message: 'Results from `map` are unused. Replace `map` with `forEach`.',
+            },
+            {
+              selector: "MemberExpression[property.name='innerText']",
+              message:
+                'The homework tests do not support `innerText`. Please replace with `textContent`.',
+            },
+            { selector: 'ForInStatement', message: 'Avoid `for in` loops' },
+          ],
+          'hyf/use-map-result': 'error',
+          'hyf/camelcase': 'warn',
+          'hyf/no-commented-out-code': 'warn',
+        },
+      };
+                        {
         // disallow numeric literals as array indices (zero is allowed)
         selector: 'MemberExpression[computed] > Literal[value>0]',
         message: 'No literal',
