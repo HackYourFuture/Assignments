64b0b4b05eac80c5f253b691f44adf94360b8f34
Nizami Mursudlu <nizamimursudlu@gmail.com>
2021-03-17 16:33:22 +0100

    exercises 1,2,3 done

--- 3-UsingAPIs/Week1/homework/ex1-johnWho.js
+++ 3-UsingAPIs/Week1/homework/ex1-johnWho.js
@@ -7,20 +7,19 @@ Rewrite this function, but replace the callback syntax with the Promise syntax:
   didn't pass in a first name!"
 ------------------------------------------------------------------------------*/
 // TODO see above
-const getAnonName = (firstName, callback) => {
-  setTimeout(() => {
-    if (!firstName) {
-      callback(new Error("You didn't pass in a first name!"));
-      return;
-    }
 
-    const fullName = `${firstName} Doe`;
-
-    callback(fullName);
-  }, 1000);
-};
-
-getAnonName('John', console.log);
+function getAnonName(firstName) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (firstName) {
+        resolve(`${firstName} Doe`)
+      } else {
+        reject(new Error("You didn't pass in a first name!"))
+      }
+    }, 1000)
+  })
+}
+getAnonName("John").then(res => console.log(res))
 
 // ! Do not change or remove the code below
 module.exports = getAnonName;
--- 3-UsingAPIs/Week1/homework/ex3-rollDice.js
+++ 3-UsingAPIs/Week1/homework/ex3-rollDice.js
@@ -10,46 +10,54 @@
 ------------------------------------------------------------------------------*/
 
 // TODO Remove callback and return a promise
-function rollDice(callback) {
-  // Compute a random number of rolls (3-10) that the dice MUST complete
-  const randomRollsToDo = Math.floor(Math.random() * 8) + 3;
-  console.log(`Dice scheduled for ${randomRollsToDo} rolls...`);
+function rollDice() {
+  return new Promise((resolve, reject) => { // Compute a random number of rolls (3-10) that the dice MUST complete
+    const randomRollsToDo = Math.floor(Math.random() * 8) + 3;
+    console.log(`Dice scheduled for ${randomRollsToDo} rolls...`);
 
-  const rollOnce = (roll) => {
-    // Compute a random dice value for the current roll
-    const value = Math.floor(Math.random() * 6) + 1;
-    console.log(`Dice value is now: ${value}`);
+    const rollOnce = (roll) => {
+      // Compute a random dice value for the current roll
+      const value = Math.floor(Math.random() * 6) + 1;
+      console.log(`Dice value is now: ${value}`);
 
-    // Use callback to notify that the dice rolled off the table after 6 rolls
-    if (roll > 6) {
-      // TODO replace "error" callback
-      callback(new Error('Oops... Dice rolled off the table.'));
-    }
+      // Use callback to notify that the dice rolled off the table after 6 rolls
+      if (roll > 6) {
+        // TODO replace "error" callback
+        return reject(new Error('Oops... Dice rolled off the table.'));
+      }
 
-    // Use callback to communicate the final dice value once finished rolling
-    if (roll === randomRollsToDo) {
-      // TODO replace "success" callback
-      callback(null, value);
-    }
+      // Use callback to communicate the final dice value once finished rolling
+      if (roll === randomRollsToDo) {
+        // TODO replace "success" callback
+        resolve(value);
+      }
 
-    // Schedule the next roll todo until no more rolls to do
-    if (roll < randomRollsToDo) {
-      setTimeout(() => rollOnce(roll + 1), 500);
-    }
-  };
+      // Schedule the next roll todo until no more rolls to do
+      if (roll < randomRollsToDo) {
+        setTimeout(() => rollOnce(roll + 1), 500);
+      }
+    };
 
-  // Start the initial roll
-  rollOnce(1);
+    // Start the initial roll
+    rollOnce(1);
+  })
+
 }
 
 // TODO Refactor to use promise
-rollDice((error, value) => {
-  if (error !== null) {
-    console.log(error.message);
-  } else {
-    console.log(`Success! Dice settled on ${value}.`);
-  }
-});
+
+rollDice()
+  .then((value) => console.log(`Success! Dice settled on ${value}.`))
+  .catch((error) => {
+    if (error !== null) {
+      console.log(error.message);
+    }
+  })
 
 // ! Do not change or remove the code below
 module.exports = rollDice;
+
+
+// i added 'return' in line 26
+// so if case error 'Dice rolled off the table.' (roll > 6)
+// it stops the function without going to the next condition
