47935a757f85a26e2890598c7d3a6627aa7b768f
robvk <r.vankruijsdijk@gmail.com>
2021-03-17 16:27:10 +0100

    Update ex4-pokerDiceAll.js
    Description was out of date

--- 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
+++ 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
@@ -1,11 +1,16 @@
 /*------------------------------------------------------------------------------
-- Run the unmodified program and confirm that problem described occurs.
-- Refactor the `rollBack()` function from callback-based to returning a
-  promise.
-- Change the calls to `callback()` to calls to `resolve()` and `reject()`.
-- Refactor the code that call `rollDice()` to use the promise it returns.
-- Does the problem described above still occur? If not, what would be your
-  explanation? Add your answer as a comment to be bottom of the file.
+For this exercise you should do the following:
+  - Refactor the rollTheDices() function to throw five dices in one go, making use of the dices array and Promise.all().
+  - A successful (i.e. resolved) throw should output a message similar to: 
+      Resolved! [ 'JACK', 'QUEEN', 'QUEEN', 'NINE', 'JACK' ]
+  - An unsuccessful (i.e. rejected) throw should output a message similar to:
+      Rejected! Dice 3 rolled off the table.
+
+The provided rollDice() function logs the value of a dice as it rolls, time-stamped with the time of day (with millisecond accuracy) to the console. 
+Once you have successfully completed this exercise you will notice that the intermediate messages are output in bursts of up to five at a time as the dices finish rolling asynchronously.
+
+You may also notice that, in the case of a rejected promise, dices that have not yet finished their roll continue to do so. 
+Can you explain why? Please add your answer as a comment to the end of the exercise file.
 ------------------------------------------------------------------------------*/
 
 // The line below makes the rollDice() function available to this file.
