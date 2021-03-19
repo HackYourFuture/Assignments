d27bd91dbc1c731f148208f8e91988cb4b14009f
Burak <isikburak142@gmail.com>
2021-03-20 00:48:17 +0100

    Completed ex2

--- 3-UsingAPIs/Week1/homework/ex2-checkDoubleDigits.js
+++ 3-UsingAPIs/Week1/homework/ex2-checkDoubleDigits.js
@@ -14,16 +14,15 @@ function checkDoubleDigits(number) {
   // TODO complete this function
   return new Promise((resolve, reject) => {
     if (number > 10 && number < 99) {
-      resolve('This is a double digit number!');
+      resolve('This is double digit number!');
     } else {
       reject(new Error(`Expected a double digit number but got ${number}`)); //This is an object
     }
   });
 }
-console.log(checkDoubleDigits(11));
 checkDoubleDigits(11) // should resolve
-  .then((message) => console.log(message)) //should has and is
-  .catch((error) => console.log(error.message)); //
+  .then((message) => console.log(message))
+  .catch((error) => console.log(error.message));
 
 checkDoubleDigits(5) // should reject
   .then((message) => console.log(message))
