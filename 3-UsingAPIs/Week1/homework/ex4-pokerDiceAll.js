c8e71689c32f38c322794d20c9b5cae13223649d
Jim Cramer <remarcmij@gmail.com>
2021-03-21 17:30:09 +0100

    Reformat comment to fit in 80 colums

--- 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
+++ 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
@@ -1,16 +1,22 @@
 /*------------------------------------------------------------------------------
 For this exercise you should do the following:
-  - Refactor the rollTheDices() function to throw five dices in one go, making use of the dices array and Promise.all().
+  - Refactor the rollTheDices() function to throw five dices in one go, making 
+    use of the dices array and Promise.all().
   - A successful (i.e. resolved) throw should output a message similar to: 
       Resolved! [ 'JACK', 'QUEEN', 'QUEEN', 'NINE', 'JACK' ]
   - An unsuccessful (i.e. rejected) throw should output a message similar to:
       Rejected! Dice 3 rolled off the table.
 
-The provided rollDice() function logs the value of a dice as it rolls, time-stamped with the time of day (with millisecond accuracy) to the console. 
-Once you have successfully completed this exercise you will notice that the intermediate messages are output in bursts of up to five at a time as the dices finish rolling asynchronously.
+The provided rollDice() function logs the value of a dice as it rolls, 
+time-stamped with the time of day (with millisecond accuracy) to the console. 
+Once you have successfully completed this exercise you will notice that the 
+intermediate messages are output in bursts of up to five at a time as the dices 
+finish rolling asynchronously.
 
-You may also notice that, in the case of a rejected promise, dices that have not yet finished their roll continue to do so. 
-Can you explain why? Please add your answer as a comment to the end of the exercise file.
+You may also notice that, in the case of a rejected promise, dices that have not
+yet finished their roll continue to do so. 
+Can you explain why? Please add your answer as a comment to the end of the 
+exercise file.
 ------------------------------------------------------------------------------*/
 
 // The line below makes the rollDice() function available to this file.
