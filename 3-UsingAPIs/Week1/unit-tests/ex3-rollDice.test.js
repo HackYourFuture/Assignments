0771f5643461e375e03fd0f686a2a1ecbe021fc2
Jim Cramer <remarcmij@gmail.com>
2021-03-23 12:49:43 +0100

    Revert changes

--- 3-UsingAPIs/Week1/unit-tests/ex3-rollDice.test.js
+++ 3-UsingAPIs/Week1/unit-tests/ex3-rollDice.test.js
@@ -2,19 +2,15 @@
 const walk = require('acorn-walk');
 const { beforeAllHelper } = require('../../../test-runner/unit-test-helpers');
 
-jest.useFakeTimers();
-
-const isPromise = (obj) => typeof obj === 'object' && 'then' in obj;
-
 describe('rollDice', () => {
   const state = {};
-  let exported, rootNode, rollDice;
+  let rootNode;
 
   beforeAll(() => {
-    ({ exported, rootNode } = beforeAllHelper(__filename, {
+    ({ rootNode } = beforeAllHelper(__filename, {
       parse: true,
+      noRequire: true,
     }));
-    rollDice = exported;
 
     rootNode &&
       walk.simple(rootNode, {
@@ -31,62 +27,15 @@ describe('rollDice', () => {
       });
   });
 
-  it('should exist and be executable', () => {
-    expect(exported).toBeDefined();
-  });
-
   it('should call `new Promise()`', () => {
-    if (!exported) return;
     expect(state.newPromise).toBeDefined();
   });
 
   it('`resolve()` should be called with a one argument', () => {
-    if (!exported) return;
     expect(state.resolve).toBe(1);
   });
 
   it('`reject()` should be called with a one argument', () => {
-    if (!exported) return;
     expect(state.reject).toBe(1);
-  });
-
-  it('should resolve if the dice settled without rolling off the table', () => {
-    if (!exported) return;
-    expect.assertions(2);
-
-    const randomSpy = jest.spyOn(Math, 'random').mockReturnValue(0);
-    const setTimeoutSpy = jest
-      .spyOn(global, 'setTimeout')
-      .mockImplementation((cb) => cb());
-
-    const promise = rollDice();
-    expect(isPromise(promise)).toBe(true);
-
-    const assertionPromise = expect(promise).resolves.toBe(1);
-
-    setTimeoutSpy.mockRestore();
-    randomSpy.mockRestore();
-
-    return assertionPromise;
-  });
-
-  it('should reject with an Error if the dice rolls off the table', () => {
-    if (!exported) return;
-    expect.assertions(2);
-
-    const randomSpy = jest.spyOn(Math, 'random').mockReturnValue(0.99);
-    const setTimeoutSpy = jest
-      .spyOn(global, 'setTimeout')
-      .mockImplementation((cb) => cb());
-
-    const promise = rollDice();
-    expect(isPromise(promise)).toBe(true);
-
-    const assertionPromise = expect(promise).rejects.toBeInstanceOf(Error);
-
-    setTimeoutSpy.mockRestore();
-    randomSpy.mockRestore();
-
-    return assertionPromise;
   });
 });
