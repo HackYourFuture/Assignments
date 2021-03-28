949c467f23eb092154c3adc3271b1b36273a70e9
Jim Cramer <remarcmij@gmail.com>
2021-03-28 17:51:11 +0200

    Update exercise comments

--- 3-UsingAPIs/Week2/homework/ex3-rollAnAce.js
+++ 3-UsingAPIs/Week2/homework/ex3-rollAnAce.js
@@ -1,8 +1,8 @@
 'use strict';
 /*------------------------------------------------------------------------------
 1. Run the unmodified exercise and observe that it works as advertised. Observe 
-   that the dice must be thrown an indeterminate number of times until we get 
-   an ACE or until it rolls off the table.
+   that the dice must be thrown an indeterminate number of times until we get an 
+   ACE or until it rolls off the table.
 2. Now, rewrite the body of the `rollDiceUntil()` function using async/await and 
    without using recursion. Hint: a `while` loop may come handy.
 3. Refactor the function `main()` to use async/await and try/catch.
