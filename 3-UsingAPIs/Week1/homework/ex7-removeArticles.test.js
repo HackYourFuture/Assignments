b240339a8dfcf6d70a9db6d6cf055103d7b800ec
Jim Cramer <remarcmij@gmail.com>
2021-04-20 15:30:08 +0200

    Add unit test exercise

---
+++ 3-UsingAPIs/Week1/homework/ex7-removeArticles.test.js
@@ -0,0 +1,26 @@
+// Grammatic articles (English)
+const articles = ['a', 'an', 'the'];
+
+const sourceText =
+  'What distinguishes Pragmatic Programmers? ' +
+  "We feel it's an attitude, a style, a philosophy of approaching problems and " +
+  'their solutions. They think beyond the immediate problem, always trying to ' +
+  'place it in its larger context, always trying to be aware of the bigger ' +
+  'picture.';
+
+const expectedText =
+  'What distinguishes Pragmatic Programmers? ' +
+  "We feel it's attitude, style, philosophy of approaching problems and " +
+  'their solutions. They think beyond immediate problem, always trying to ' +
+  'place it in its larger context, always trying to be aware of bigger ' +
+  'picture.';
+
+function removeArticles(text) {
+  // TODO replace with your own code
+}
+describe('removeArticle', () => {
+  it('should remove all articles from a string of English text', () => {
+    // TODO replace with your own code
+    expect(false).toBe(true);
+  });
+});
