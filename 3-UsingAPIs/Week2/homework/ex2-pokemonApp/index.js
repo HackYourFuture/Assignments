ed53b5cdf4764d2978609de5ca8c68846519fc06
erk.mustech <erk.mustech@gmail.com>
2021-06-21 00:19:41 +0200

    ex2 done

--- 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
+++ 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
-const { forEach } = require('lodash');
-const { Body } = require('node-fetch');
+// const { doc } = require('prettier');
+
+// const { forEach } = require('lodash');
+// const { Body } = require('node-fetch');
 
 /*------------------------------------------------------------------------------
 Complete the four functions provided in the starter `index.js` file:
@@ -24,26 +26,75 @@ Use async/await and try/catch to handle promises.
 Try and avoid using global variables. As much as possible, try and use function 
 parameters and return values to pass data back and forth.
 ------------------------------------------------------------------------------*/
+
+const url = 'https://pokeapi.co/api/v2/pokemon?limit=156';
+const arr = [];
 async function fetchData(url) {
   try {
     const response = await fetch(url);
-    return response.json();
+    if (response.ok) {
+      const data = await response.json();
+      for (let i = 0; i < 156; i++) {
+        arr.push(data.results[i].name);
+      }
+      return arr;
+    }
   } catch (error) {
     console.log(error);
   }
 }
 
-function fetchAndPopulatePokemons(/* TODO parameter(s) go here */) {
+function fetchAndPopulatePokemons(data) {
   // TODO complete this function
-  const img = document.createElement('img');
-  document.querySelectorAll('body').appendChild(select);
+  const body = document.querySelector('body');
+  body.style.display = 'flex';
+  const btn = document.createElement('button');
+  btn.textContent = 'Get Pokemon';
+  body.appendChild(btn);
+  // btn.addEventListener('click' fetchData());
+
+  const selectList = document.createElement('select');
+  selectList.id = 'mySelect';
+  body.appendChild(selectList);
+  console.log(arr);
+
+  // let leng = data.results.length;
+  // console.log(leng);
+  // const arr = push(data.results);
+  // console.log(arr.length);
+
+  for (let i = 0; i < arr.length; i++) {
+    const option = document.createElement('option');
+    option.value = arr[i];
+    option.text = arr[i];
+    selectList.appendChild(option);
+  }
 }
 
 function fetchImage(/* TODO parameter(s) go here */) {
   // TODO complete this function
-  const imgScr = response.img;
+  const img = document.createElement('img');
+  img.src = data.img;
+  document.querySelector('body').appendChild(img);
+  console.log(data.img);
 }
 
 function main() {
   // TODO complete this function
+  const displaySuggestions = (event) => {
+    event.preventDefault();
+    while (responseField.firstChild) {
+      responseField.removeChild(responseField.firstChild);
+    }
+    getSuggestions();
+  };
+
+  submit.addEventListener('click', displaySuggestions);
 }
+
+fetchAndPopulatePokemons();
+fetchData(url);
+
+// window.onload = function () {
+//   main();
+// };
