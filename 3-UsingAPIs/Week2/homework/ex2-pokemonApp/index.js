1dbcb2b2e2abd464833cb1eb0d27ca7b9839c6d3
erk.mustech <erk.mustech@gmail.com>
2021-06-22 22:06:49 +0200

    final work

--- 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
+++ 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
@@ -47,34 +47,50 @@ function fetchAndPopulatePokemons(data) {
     const option = document.createElement('option');
     option.value = item.name;
     option.textContent = item.name;
-    option.url = item.ul;
-    option.addEventListener('click', async () => {
-      const link = await fetchData(url);
-      fetchImage(link);
-    });
     selectList.appendChild(option);
+    option.addEventListener('click', async () => {
+      await fetchImage(item);
+    });
   });
 }
 
 function fetchImage(data) {
-  const img = document.getElementById('img');
-  img.src = data.url;
-  body.appendChild(img);
+  const imgUrl = 'https://pokeapi.co/api/v2/pokemon/';
+  let queryParam = '1';
+  const lastPram = '/';
+  const endpoint = `${imgUrl}${queryParam}${lastPram}`;
+  let imgData = fetch(endpoint);
+  const imgRes = imgData.json();
+  const imgName = imgRes.name;
+  const imgSrc = imgRes.sprites.back_default;
+
+  while (imgName !== data.name) {
+    for (let i = 2; i < 151; i++) {
+      queryParam = i;
+      imgData = fetch(endpoint);
+    }
+  }
+  const imgBox = document.createElement('div');
+  const img = document.createElement('img');
+  imgBox.appendChild('img');
+  body.appendChild(imgBox);
+  img.src = imgSrc;
 }
+//unfortunately the pic can't show up in screen. I can't solve. I will wait for your hint.
 
 function main() {
   body.style.display = 'flex';
   const btn = document.createElement('button');
-  btn.textContent = 'Get Pokemon';
+  btn.textContent = 'Get Pokemon!';
   btn.type = 'submit';
   body.appendChild(btn);
+  const selectList = document.createElement('select');
+  selectList.id = 'mySelect';
+  body.appendChild(selectList);
   btn.addEventListener('click', async () => {
     const res = await fetchData(url);
     fetchAndPopulatePokemons(res);
   });
-  const selectList = document.createElement('select');
-  selectList.id = 'mySelect';
-  body.appendChild(selectList);
 }
 
 window.addEventListener('load', main);
