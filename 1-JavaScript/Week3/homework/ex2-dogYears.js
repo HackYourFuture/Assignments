8cb0e28c9f2e2baebc5b43c6f44d28b1dce3a40c
Alejandro Urroz <alejandrourroz86@gmail.com>
2021-07-10 01:34:20 +0200

    Resolved exercise 2 dog years

--- 1-JavaScript/Week3/homework/ex2-dogYears.js
+++ 1-JavaScript/Week3/homework/ex2-dogYears.js
@@ -14,8 +14,8 @@ calculate it!
    ages.
 -----------------------------------------------------------------------------*/
 
-function calculateDogAge(/* parameters go here */) {
-  // TODO complete this function
+function calculateDogAge(number) {
+  return 'Your doggie is ' + number * 7 + ' years old in dog years!' ;
 }
 
 console.log(calculateDogAge(1)); // -> "Your doggie is 7 years old in dog years!"
