4a30460d0df6d89eea3bb8c5ed5b457b75c82f1b
Alejandro Urroz <alejandrourroz86@gmail.com>
2021-07-10 12:26:09 +0200

    Modified return statement by adding template literals

--- 1-JavaScript/Week3/homework/ex2-dogYears.js
+++ 1-JavaScript/Week3/homework/ex2-dogYears.js
@@ -15,7 +15,8 @@ calculate it!
 -----------------------------------------------------------------------------*/
 
 function calculateDogAge(number) {
-  return 'Your doggie is ' + number * 7 + ' years old in dog years!' ;
+  let dogYear = numer * 7;
+  return `Your doggie is ${dogYear} years old in dog years!`;
 }
 
 console.log(calculateDogAge(1)); // -> "Your doggie is 7 years old in dog years!"
