e03f95ecc9f3a1d03c4d90a51341c20a13993a24
Jim Cramer <remarcmij@gmail.com>
2021-08-18 13:06:37 +0200

    Text corrections

--- 1-JavaScript/Week3/homework/ex4-shoppingCart.js
+++ 1-JavaScript/Week3/homework/ex4-shoppingCart.js
@@ -28,7 +28,7 @@ function addToShoppingCart(/* parameters go here */) {
 // ! Test functions (plain vanilla JavaScript)
 function test1() {
   console.log(
-    'Test 0: addShoppingCart() called without an argument should leave the shopping cart unchanged'
+    'Test 1: addShoppingCart() called without an argument should leave the shopping cart unchanged'
   );
   const expected = 'You bought bananas, milk!';
   const actual = addToShoppingCart();
@@ -36,28 +36,28 @@ function test1() {
 }
 
 function test2() {
-  console.log('Test 1: addShoppingCart() should take one parameter');
+  console.log('Test 2: addShoppingCart() should take one parameter');
   const expected = 1;
   const actual = addToShoppingCart.length;
   console.assert(actual === expected);
 }
 
 function test3() {
-  console.log('Test 2: `chocolate` should be added');
+  console.log('Test 3: `chocolate` should be added');
   const expected = 'You bought bananas, milk, chocolate!';
   const actual = addToShoppingCart('chocolate');
   console.assert(actual === expected);
 }
 
 function test4() {
-  console.log('Test 3: `waffles` should be added and `bananas` removed');
+  console.log('Test 4: `waffles` should be added and `bananas` removed');
   const expected = 'You bought milk, chocolate, waffles!';
   const actual = addToShoppingCart('waffles');
   console.assert(actual === expected);
 }
 
 function test5() {
-  console.log('Test 4: `tea` should be added and `milk` removed');
+  console.log('Test 5: `tea` should be added and `milk` removed');
   const expected = 'You bought chocolate, waffles, tea!';
   const actual = addToShoppingCart('tea');
   console.assert(actual === expected);
