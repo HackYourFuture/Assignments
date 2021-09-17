ccdb27344b47ca9859e05c8e41d5e672a6566ce1
Jim Cramer <remarcmij@gmail.com>
2021-09-17 13:43:16 +0200

    Improve rollAnAce unit test

--- 3-UsingAPIs/Week2/unit-tests/ex3-rollAnAce.test.js
+++ 3-UsingAPIs/Week2/unit-tests/ex3-rollAnAce.test.js
@@ -7,13 +7,14 @@ const {
 
 describe('ex3-rollAnAce', () => {
   const state = {};
-  let rootNode, source;
+  let exported, rootNode, rollDiceUntil, source;
 
   beforeAll(async () => {
-    ({ rootNode, source } = beforeAllHelper(__filename, {
-      noRequire: true,
+    ({ rootNode, exported, source } = beforeAllHelper(__filename, {
       parse: true,
     }));
+
+    rollDiceUntil = exported;
 
     rootNode &&
       walk.simple(rootNode, {
@@ -42,5 +43,78 @@ describe('ex3-rollAnAce', () => {
 
   test('should use try/catch', () => {
     expect(state.tryCatch).toBeDefined();
+  });
+
+  test('should resolve as soon as a dice settles on an ACE', async () => {
+    expect.assertions(3);
+
+    expect(exported).toBeDefined();
+
+    const logSpy = jest.spyOn(console, 'log').mockImplementation();
+
+    // This sequence is known to settle on an ACE in three throws.
+    const randomSpy = jest
+      .spyOn(global.Math, 'random')
+      .mockReturnValueOnce(0.1992491366159479)
+      .mockReturnValueOnce(0.9146104190986211)
+      .mockReturnValueOnce(0.3023371194859603)
+      .mockReturnValueOnce(0.6134982380471312)
+      .mockReturnValueOnce(0.44118936387972973)
+      .mockReturnValueOnce(0.43394953147951876)
+      .mockReturnValueOnce(0.23036088287785983)
+      .mockReturnValueOnce(0.14003818771402154)
+      .mockReturnValueOnce(0.2066946242859471)
+      .mockReturnValueOnce(0.2644310391073186)
+      .mockReturnValueOnce(0.8856626254905846)
+      .mockReturnValueOnce(0.6403424753337192);
+    const setTimeoutSpy = jest
+      .spyOn(global, 'setTimeout')
+      .mockImplementation((cb) => cb());
+
+    try {
+      const promise = rollDiceUntil('ACE');
+      expect(promise).toBeInstanceOf(Promise);
+      const result = await promise;
+      expect(result).toBe('ACE');
+    } finally {
+      setTimeoutSpy.mockRestore();
+      randomSpy.mockRestore();
+      logSpy.mockRestore();
+    }
+  });
+
+  test('should reject with an Error when a dice rolls off the table', async () => {
+    expect.assertions(3);
+    expect(exported).toBeDefined();
+
+    const logSpy = jest.spyOn(console, 'log').mockImplementation();
+
+    // This sequence is known to cause the dice to roll off the table
+    // in two throws.
+    const randomSpy = jest
+      .spyOn(global.Math, 'random')
+      .mockReturnValueOnce(0.3453916441274625)
+      .mockReturnValueOnce(0.9834560635274558)
+      .mockReturnValueOnce(0.46197066731784786)
+      .mockReturnValueOnce(0.14310034044692732)
+      .mockReturnValueOnce(0.13920562773159428)
+      .mockReturnValueOnce(0.4756738537780816)
+      .mockReturnValueOnce(0.38174035952881447)
+      .mockReturnValueOnce(0.7714732722437749);
+    const setTimeoutSpy = jest
+      .spyOn(global, 'setTimeout')
+      .mockImplementation((cb) => cb());
+
+    try {
+      const promise = rollDiceUntil('ACE');
+      expect(promise).toBeInstanceOf(Promise);
+      await promise;
+    } catch (err) {
+      expect(err).toBeInstanceOf(Error);
+    } finally {
+      setTimeoutSpy.mockRestore();
+      randomSpy.mockRestore();
+      logSpy.mockRestore();
+    }
   });
 });
