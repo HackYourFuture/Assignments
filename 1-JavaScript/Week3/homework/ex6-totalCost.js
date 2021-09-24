2bd83e0cb6598a767bed0caf097e5278b35a2be6
Pedro Ramos <pedroramosromero@gmail.com>
2021-09-24 19:21:36 +0200

    Exercise 6 completed and tests passed.

--- 1-JavaScript/Week3/homework/ex6-totalCost.js
+++ 1-JavaScript/Week3/homework/ex6-totalCost.js
@@ -22,20 +22,36 @@ instead!
 -----------------------------------------------------------------------------*/
 const cartForParty = {
   // TODO complete this object
+  beers: 1.75,
+  chips: 0.99,
+  olives: 2.55,
+  vermouth: 6.40,
+  ice: 1.50
 };
 
-function calculateTotalPrice(/* TODO parameter(s) go here */) {
+function calculateTotalPrice(cart) {
   // TODO replace this comment with your code
+  let amount = 0;
+  // eslint-disable-next-line no-restricted-syntax
+  for (const i in cart) {
+    amount += cart[i];
+  }
+  return `Total: €${amount}`;
 }
 
 function test1() {
   console.log('\nTest 1: calculateTotalPrice should take one parameter');
   // TODO replace this comment with your code
+  console.assert(calculateTotalPrice.length === 1);
 }
 
 function test2() {
   console.log('\nTest 2: return correct output when passed cartForParty');
   // TODO replace this comment with your code
+  const amountExpected = cartForParty.beers + cartForParty.chips + cartForParty.olives + cartForParty.vermouth + cartForParty.ice;
+  const expectedResult = `Total: €${amountExpected}`;
+  const actualResult = calculateTotalPrice(cartForParty);
+  console.assert(expectedResult === actualResult);
 }
 
 function test() {
