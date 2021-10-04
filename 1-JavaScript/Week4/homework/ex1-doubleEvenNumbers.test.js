48e74c6170fef651f462bcb0c8aaafb0e4479f7e
Pedro Ramos <pedroramosromero@gmail.com>
2021-10-04 21:47:18 +0200

    Exercise 1 completed. All tests passed.

--- 1-JavaScript/Week4/homework/ex1-doubleEvenNumbers.test.js
+++ 1-JavaScript/Week4/homework/ex1-doubleEvenNumbers.test.js
@@ -12,12 +12,8 @@ Let's rewrite it (or _refactor_ it, as experienced developers would call it):
 // ! Function to be tested
 function doubleEvenNumbers(numbers) {
   // TODO rewrite the function body using `map` and `filter`.
-  const newNumbers = [];
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 === 0) {
-      newNumbers.push(numbers[i] * 2);
-    }
-  }
+  const evenNumbers = numbers.filter(number => number % 2 === 0);
+  const newNumbers = evenNumbers.map(evenNumber => evenNumber * 2);
   return newNumbers;
 }
 
