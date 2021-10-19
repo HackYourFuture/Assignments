11499c587fd26675b646a7d8318969aa3f7ddbeb
Daniel Wang <Daniel.y.wang95@gmail.com>
2021-10-19 15:50:15 +0200

    homework 1 completed

--- 1-JavaScript/Week3/homework/ex1-giveCompliment.js
+++ 1-JavaScript/Week3/homework/ex1-giveCompliment.js
@@ -18,18 +18,35 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/1-Java
    Use `console.log` each time to display the return value of the
    `giveCompliment` function to the console.
 -----------------------------------------------------------------------------*/
-function giveCompliment(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function giveCompliment(name) {
+  const greetArr = [
+    'awesome ',
+    'tech-savvy ',
+    'nerdy ',
+    `lovely `,
+    'funny ',
+    'romantic ',
+    'dorky ',
+    'kind ',
+    'benevolent ',
+    'joyful ',
+  ];
+  return (
+    'You are ' +
+    greetArr[Math.floor(Math.random() * greetArr.length)] +
+    name +
+    '!'
+  );
 }
 
 // TODO substitute your own name for "HackYourFuture"
-const myName = 'HackYourFuture';
+const myName = 'Danny';
 
 console.log(giveCompliment(myName));
 console.log(giveCompliment(myName));
 console.log(giveCompliment(myName));
 
-const yourName = 'Amsterdam';
+const yourName = 'Bobby';
 
 console.log(giveCompliment(yourName));
 console.log(giveCompliment(yourName));
