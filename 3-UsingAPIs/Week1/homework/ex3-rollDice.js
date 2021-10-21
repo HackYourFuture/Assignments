e87c19a19cda2e916396364059528f75d86d15b3
Pedro Ramos <pedroramosromero@gmail.com>
2021-10-21 15:56:59 +0200

    Exercise 3 completed. All unit tests passed.

--- 3-UsingAPIs/Week1/homework/ex3-rollDice.js
+++ 3-UsingAPIs/Week1/homework/ex3-rollDice.js
@@ -11,47 +11,46 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/3-Usin
   explanation? Add your answer as a comment to be bottom of the file.
 ------------------------------------------------------------------------------*/
 
-// TODO Remove callback and return a promise
-function rollDice(callback) {
-  // Compute a random number of rolls (3-10) that the dice MUST complete
-  const randomRollsToDo = Math.floor(Math.random() * 8) + 3;
-  console.log(`Dice scheduled for ${randomRollsToDo} rolls...`);
+function rollDice() {
 
-  const rollOnce = (roll) => {
-    // Compute a random dice value for the current roll
-    const value = Math.floor(Math.random() * 6) + 1;
-    console.log(`Dice value is now: ${value}`);
+  return new Promise((resolve, reject) => {
+    // Compute a random number of rolls (3-10) that the dice MUST complete
+    const randomRollsToDo = Math.floor(Math.random() * 8) + 3;
+    console.log(`Dice scheduled for ${randomRollsToDo} rolls...`);
 
-    // Use callback to notify that the dice rolled off the table after 6 rolls
-    if (roll > 6) {
-      // TODO replace "error" callback
-      callback(new Error('Oops... Dice rolled off the table.'));
-    }
+    const rollOnce = (roll) => {
 
-    // Use callback to communicate the final dice value once finished rolling
-    if (roll === randomRollsToDo) {
-      // TODO replace "success" callback
-      callback(null, value);
-    }
+      // Notify that the dice rolled off the table after 6 rolls
+      if (roll > 6) {
+        reject(new Error('Oops... Dice rolled off the table.'));
+        return;
+      }
 
-    // Schedule the next roll todo until no more rolls to do
-    if (roll < randomRollsToDo) {
-      setTimeout(() => rollOnce(roll + 1), 500);
-    }
-  };
+      // Compute a random dice value for the current roll
+      const value = Math.floor(Math.random() * 6) + 1;
+      console.log(`Dice value is now: ${value}`);
 
-  // Start the initial roll
-  rollOnce(1);
+      // Communicate the final dice value once finished rolling
+      if (roll === randomRollsToDo) {
+        resolve(`Success! Dice settled on ${value}.`);
+      }
+
+      // Schedule the next roll todo until no more rolls to do
+      if (roll < randomRollsToDo) {
+        setTimeout(() => rollOnce(roll + 1), 500);
+      }
+    };
+
+    // Start the initial roll
+    rollOnce(1);
+
+  });
+
 }
 
-// TODO Refactor to use promise
-rollDice((error, value) => {
-  if (error !== null) {
-    console.log(error.message);
-  } else {
-    console.log(`Success! Dice settled on ${value}.`);
-  }
-});
+rollDice()
+  .then((message) => console.log(message))
+  .catch((error) => console.log(error.message));
 
 // ! Do not change or remove the code below
 module.exports = rollDice;
