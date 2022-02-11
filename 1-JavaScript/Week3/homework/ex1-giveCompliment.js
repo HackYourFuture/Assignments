8eb6b9715df4d0e56bce9899e8563379c9bf2da5
lynn@aspire1 <lynn@aspire1>
2022-02-11 01:18:07 +0100

    modified ex1 and ex2

--- 1-JavaScript/Week3/homework/ex1-giveCompliment.js
+++ 1-JavaScript/Week3/homework/ex1-giveCompliment.js
@@ -18,13 +18,26 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/1-Java
    Use `console.log` each time to display the return value of the
    `giveCompliment` function to the console.
 -----------------------------------------------------------------------------*/
-function giveCompliment(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function giveCompliment(name) {
+  const compliments = [
+    'awesome',
+    'amazing ',
+    'great',
+    'smart',
+    'cute',
+    'beautiful',
+    'genius',
+    'funny',
+    'intellectual',
+    'sophisticated',
+  ];
+  const random = Math.floor(Math.random() * 10);
+  console.log('You are ' + compliments[random] + ' ' + name + '!');
 }
 
 function main() {
-  // TODO substitute your own name for "HackYourFuture"
-  const myName = 'HackYourFuture';
+  
+  const myName = 'Lynn';
 
   console.log(giveCompliment(myName));
   console.log(giveCompliment(myName));
--- 1-JavaScript/Week3/homework/ex2-dogYears.js
+++ 1-JavaScript/Week3/homework/ex2-dogYears.js
@@ -16,8 +16,9 @@ calculate it!
    ages.
 -----------------------------------------------------------------------------*/
 
-function calculateDogAge(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function calculateDogAge(age) {
+  const convertedAge = age * 7;
+  return 'Your doggie is ' + convertedAge + ' years old in dog years!';
 }
 
 function main() {
