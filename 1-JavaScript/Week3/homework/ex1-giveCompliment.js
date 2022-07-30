3696e8fd0009c4493a9497341b5c34dffe60fd94
Hikmet Dag <temkihgad@gmail.com>
2022-07-30 18:52:32 +0200

    Week 3 exercise 1

--- 1-JavaScript/Week3/homework/ex1-giveCompliment.js
+++ 1-JavaScript/Week3/homework/ex1-giveCompliment.js
@@ -18,13 +18,15 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/1-Java
    Use `console.log` each time to display the return value of the
    `giveCompliment` function to the console.
 -----------------------------------------------------------------------------*/
-function giveCompliment(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function giveCompliment(yourName) {
+  const Array=['ok','great','fantastic','Excellent','super','very good']
+  const x=Math.floor(Math.random()*Array.length);
+  return `You are ${Array[x]}, ${yourName}`
 }
 
 function main() {
   // TODO substitute your own name for "HackYourFuture"
-  const myName = 'HackYourFuture';
+  const myName = 'Hikmet';
 
   console.log(giveCompliment(myName));
   console.log(giveCompliment(myName));
