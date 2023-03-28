c2736876040c028cb923b9510430cda25d68b6f9
Ethel Chila <chilachila016@gmail.com>
2023-03-28 17:30:04 +0200

    completed exercise 1

---
+++ ex1-giveCompliment.js
@@ -0,0 +1,58 @@
+'use strict';
+/* -----------------------------------------------------------------------------
+Full description at: https://github.com/HackYourFuture/Homework/tree/main/1-JavaScript/Week3#exercise-1-you-are-amazing
+
+1. Complete the function named `giveCompliment`as follows:
+
+   - It should take a single parameter: `name`.
+   - Its function body should include a variable that holds an array,
+     `compliments`, initialized with 10 strings. Each string should be a
+     compliment, like `"great"`, `"awesome"` and so on.
+   - It should randomly select a compliment from the array.
+   - It should return the string "You are `compliment`, `name`!", where
+     `compliment` is a randomly selected compliment and `name` is the name that
+     was passed as an argument to the function.
+
+2. Call the function three times, giving each function call the same argument:
+   your name.
+   Use `console.log` each time to display the return value of the
+   `giveCompliment` function to the console.
+-----------------------------------------------------------------------------*/
+function giveCompliment(name) {
+  // TODO complete this function
+let compliments = [
+    "Beautiful",
+    "Intelligent",
+    "Confident",
+    "Awesome",
+    "Amazing",
+    "Focused",
+    "Creative",
+    "Lovely",
+    "Smart",
+    "Great",
+  ];
+  let random = compliments[Math.floor(Math.random() * compliments.length)];
+  return `You are ${random}, ${name}!`;
+}
+
+function main() {
+  // TODO substitute your own name for "HackYourFuture"
+  const myName = 'Ethel';
+
+  console.log(giveCompliment(myName));
+  console.log(giveCompliment(myName));
+  console.log(giveCompliment(myName));
+
+  const yourName = 'Amsterdam';
+
+  console.log(giveCompliment(yourName));
+  console.log(giveCompliment(yourName));
+  console.log(giveCompliment(yourName));
+}
+
+// ! Do not change or remove the code below
+if (process.env.NODE_ENV !== 'test') {
+  main();
+}
+module.exports = giveCompliment;
