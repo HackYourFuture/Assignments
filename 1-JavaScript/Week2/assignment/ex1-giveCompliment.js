5e5985a80f492ae0833f83c8ed19aef50ffbfea9
HackYourFuture <hackyourfuture@hackyouturesair.home>
2024-11-29 00:07:57 +0100

    ex1 completed

--- 1-JavaScript/Week2/assignment/ex1-giveCompliment.js
+++ 1-JavaScript/Week2/assignment/ex1-giveCompliment.js
@@ -3,34 +3,54 @@ Full description at: https://github.com/HackYourFuture/Assignments/tree/main/1-J
 
 1. Complete the function named `giveCompliment`as follows:
 
-   - It should take a single parameter: `name`.
+   - It should take a single parameter: name.
    - Its function body should include a variable that holds an array,
-     `compliments`, initialized with 10 strings. Each string should be a
-     compliment, like `"great"`, `"awesome"` and so on.
+     compliments, initialized with 10 strings. Each string should be a
+     compliment, like "great", "awesome" and so on.
    - It should randomly select a compliment from the array.
-   - It should return the string "You are `compliment`, `name`!", where
-     `compliment` is a randomly selected compliment and `name` is the name that
+   - It should return the string "You are compliment, name!", where
+     compliment is a randomly selected compliment and name is the name that
      was passed as an argument to the function.
 
 2. Call the function three times, giving each function call the same argument:
    your name.
-   Use `console.log` each time to display the return value of the
-   `giveCompliment` function to the console.
+   Use console.log each time to display the return value of the
+   giveCompliment function to the console.
 -----------------------------------------------------------------------------*/
-export function giveCompliment(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+export function giveCompliment(name) {
+  //Array of compliments names
+  const compliments = [
+    'amazing',
+    'awesome',
+    'brilliant',
+    'clever',
+    'wonderful',
+    'excellent',
+    'fantastic',
+    'funny',
+    'smart',
+    'good',
+  ];
+  // Randomly select a compliment
+  const randomIndex = Math.floor(Math.random() * compliments.length);
+  const compliment = compliments[randomIndex];
+
+  // Return the compliment string
+  return `You are ${compliment}, ${name}!`;
 }
 
 function main() {
-  // TODO substitute your own name for "HackYourFuture"
-  const myName = 'HackYourFuture';
+  const myName = 'Hossein';
 
+  // Call the function three times with myName
   console.log(giveCompliment(myName));
   console.log(giveCompliment(myName));
   console.log(giveCompliment(myName));
 
-  const yourName = 'Amsterdam';
+  // Another example with a different name
+  const yourName = 'Kelisa';
 
+  // Call the function three times with yourName
   console.log(giveCompliment(yourName));
   console.log(giveCompliment(yourName));
   console.log(giveCompliment(yourName));
